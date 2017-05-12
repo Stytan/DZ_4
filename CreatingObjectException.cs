5c469f07ab52dc16723035efda7fd513cb4ab3ff
Stytan <stytanus@gmail.com>
2017-05-12 16:38:37 +0300

    Create CreatingObjectException.cs

---
+++ CreatingObjectException.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace DZ_4
+{
+	[Serializable]
+	/// <summary>
+	/// Description of CreatingObjectException.
+	/// </summary>
+	public class CreatingObjectException : ApplicationException
+	{
+		public CreatingObjectException() : base("Can't create a figure with the specified parameters") { }
+
+        public CreatingObjectException(string message) : base(message) { }
+
+        public CreatingObjectException(string message, Exception inner) : base(message, inner) { }
+
+        protected CreatingObjectException(System.Runtime.Serialization.SerializationInfo info,
+                                        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+	}
+}
