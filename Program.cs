ab0c94592591f961d4b7e25d89ec5d301f5f597f
Stytan <stytanus@gmail.com>
2017-05-15 16:30:05 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,9 +6,28 @@ namespace DZ_4
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var Figures = new CompoundFigure();
+			Figures.Figures.Add(new Parallelogram(1555, 23, (float)90));
+			Figures.Figures.Add(new Parallelogram(1555, 23, 23.0));
+			Figures.Figures.Add(new Trapeze(20, 15, 5, 35));
+			Figures.Figures.Add(new Trapeze(15, 20, 5, 35));
+			Figures.Figures.Add(new Ellipse(20, 30));
+			Figures.Figures.Add(new Circle(18));
+			Figures.Figures.Add(new Foursquare(32));
+			Figures.Figures.Add(new Rectangle(16, 35.6));
+			Figures.Figures.Add(new Rhombus(21, 26));
+			Figures.Figures.Add(new Triangle(8, 15, 12));
 			
-			// TODO: Implement Functionality Here
+			foreach(GeometricFigure Figure in Figures)
+			{
+				Console.WriteLine(Figure);
+				Console.WriteLine("Height = {0:0,#}, Base = {1:0,#}, Angle = {2:0,#}, " +
+				"Count of side = {3:0,#}",
+					(Figure as SimpleNgon).Height,
+					(Figure as SimpleNgon).Baze,
+					(Figure as SimpleNgon).BaseToSideAngle,
+					(Figure as SimpleNgon).CountOfSides);
+			}
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
